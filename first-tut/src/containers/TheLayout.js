0e536b2be19cb0bdbe9e470b0480650a06ef3423
lazzeri <lucalazzeri97@gmail.com>
2020-09-12 19:04:19 +0200

    Add Downloaded Template

---
+++ first-tut/src/containers/TheLayout.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {
+  TheContent,
+  TheSidebar,
+  TheFooter,
+  TheHeader
+} from './index'
+
+const TheLayout = () => {
+
+  return (
+    <div className="c-app c-default-layout">
+      My first react Website
+    </div>
+  )
+}
+
+export default TheLayout
